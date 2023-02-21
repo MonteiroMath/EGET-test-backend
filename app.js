febf00a5826cb8a80a2069e886a5ba99235742a2
Matheus Antunes Monteiro <math.am@hotmail.com>
2023-02-21 10:05:16 -0300

    removed bodyParser - deprecated

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 var createError = require("http-errors");
 var express = require("express");
 var path = require("path");
-var bodyParser = require("body-parser");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 
@@ -15,7 +14,6 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(bodyParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", productsRouter);
