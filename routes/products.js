9eb7783afc19f516a7a6aababd07b3f3e4dd0379
Matheus Antunes Monteiro <math.am@hotmail.com>
2023-02-21 09:24:49 -0300

    initial draft of routes

--- routes/products.js
+++ routes/products.js
@@ -2,7 +2,30 @@ var express = require("express");
 var router = express.Router();
 
 router.get("/", function (req, res, next) {
-  res.json("hello");
+  //return a list of all products or of those that satisfy the query
+  res.json([]);
+});
+
+router.post("/", function (req, res, next) {
+  //save a new product to the db
+  res.json("");
+});
+
+router.get("/:id", function (req, res, next) {
+  //get a specific product of id :id
+
+  res.json("");
+});
+
+router.put("/:id", function (req, res, next) {
+  //alters a specific product of id :id
+  res.json("");
+});
+
+router.delete("/:id", function (req, res, next) {
+  //delete a specific product of id :id
+
+  res.json("");
 });
 
 module.exports = router;
